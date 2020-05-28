e5ba7683bde244f4a19d902e275b338210ebb9a1
rlacroix2331 <59836289+rlacroix2331@users.noreply.github.com>
2020-05-28 17:05:38 +0000

    Add project files.

---
+++ BooksMVC/Domain/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksMVC.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+        public bool InInventory { get; set; }
+    }
+}
