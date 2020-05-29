81e568934ebbc7a6d8c2f23f29caa089b7062693
rlacroix2331 <59836289+rlacroix2331@users.noreply.github.com>
2020-05-29 15:35:12 +0000

    day 2 morning updates

--- BooksMVC/Controllers/BooksController.cs
+++ BooksMVC/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 //using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using BooksMVC.Domain;
 using BooksMVC.Models.Books;
@@ -18,9 +19,60 @@ namespace BooksMVC.Controllers
         {
             _dataContext = dataContext;
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            // TODO:If it isn't there, send a 404
+            var bookToEdit = await _dataContext.Books
+                .SingleOrDefaultAsync(b => b.Id == id);
+            //    .Select(b => new BookEdit
+            //    {
+            //        Id = b.Id,
+            //        Title = b.Title,
+            //        Author = b.Author,
+            //        NumberOfPages = b.NumberOfPages,
+            //        PublicationDate = new DateTime(1969, 4, 20)
+            //    });
+
+            return View(bookToEdit);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(BookEdit editedBook)
+        {
+            // Todo: Validate it, update it, redirect
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View(new BookCreate());//return view to add a new book
+        }
+        [HttpPost("/books")]
+        public async Task<IActionResult> Create(BookCreate bookToAdd)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("New", bookToAdd);
+            }
+            else
+            {
+                var book = new Book
+                {
+                    Title = bookToAdd.Title,
+                    Author = bookToAdd.Author,
+                    InInventory = true,
+                    NumberOfPages = bookToAdd.NumberOfPages
+                };
+                _dataContext.Books.Add(book);
+                await _dataContext.SaveChangesAsync();
+                ViewData["flash"] = $"Book {book.Title} add as {book.Id}";
+                return View("New", new BookCreate());//PRG post redirect get
+            }
+        }
         [HttpGet("/books/{bookId:int}")]
         public async Task<IActionResult> Details(int bookId)
         {
+            
             var response = await _dataContext.Books.Where(b => b.Id == bookId)
                 .Select(b => new GetSingleBookResponseModel
                 {
@@ -48,6 +100,7 @@ namespace BooksMVC.Controllers
             //no model, just serilizing the domain objextsvart
             //var response = await _dataContext.Books.Where(b => b.InInventory).ToListAsync();
             //return View(response);
+            ViewData["sale"] = "all books on sale";
             var response = new GetBooksResponseModel
             {
                 Books = await _dataContext.Books.Where(b => b.InInventory).Select(b => new BooksResponseItemModel
---
+++ BooksMVC/Models/Books/BookCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksMVC.Models.Books
+{
+    public class BookCreate : IValidatableObject
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [DisplayName("Number of pages")]
+        [Range(1, int.MaxValue, ErrorMessage ="Number of pages is required")]
+        public int NumberOfPages { get; set; }
+        // [DataType(DateType.Date)]
+        public DateTime PublicationDate { get; set; } = DateTime.Now;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Title.ToLower().EndsWith("king") && Title.ToLower() == "it")
+            {
+                yield return new ValidationResult("Not that boo please",
+                    new string[] { nameof(Title), nameof(Author)});
+            }
+        }
+    }
+}
